812c5a37f4a11e6725662b349fdc361a269237a8
ciscons <cisconamsu@gmail.com>
2022-07-28 10:10:33 +0900

    third commit 0728
    Git ignore 파일 수정

--- src/main/java/com/simple/basic/controller/ValidController.java
+++ src/main/java/com/simple/basic/controller/ValidController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import javax.validation.Valid;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -23,14 +24,22 @@ public class ValidController {
 	
 	// vo에 유효성 검사를 진행하고, 실패하면 Errors 객체로 바인딩 된다.
 	@PostMapping("/validForm")
-	public String validForm(@Valid ValidVO vo, Errors errors ) {
+	public String validForm(@Valid ValidVO vo, Errors errors, Model model ) {
 		
 		if(errors.hasErrors()) {
 			List<FieldError> list = errors.getFieldErrors();
-			System.out.println(list);
+//			System.out.println(list);
+			
+			for(FieldError err : list) {
+//				System.out.println("실패한 필드명: " + err.getField());
+//				System.out.println("실패한 필드의 메시지: " + err.getDefaultMessage());
+//				System.out.println("바인딩 실패 여부: " + err.isBindingFailure()); // data type이 달라서 아예 검사조차 되지 않은 경우
+				
+				model.addAttribute("valid_" + err.getField(), err.getDefaultMessage());
+			}
+			model.addAttribute("vo", vo);
+			return "valid/view"; //유효성검사를 성공시 나가는화면
 		}
-		
-		
 		return "valid/result"; //유효성검사를 성공시 나가는화면
 	}
 	
